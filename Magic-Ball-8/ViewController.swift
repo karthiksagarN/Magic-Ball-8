856849ed40d7d8d307b5c056cde41f7f22d80c41
karthiksagarN <karthik.sagarn@gmail.com>
2024-04-09 02:12:49 +0530

    Initial Commit

---
+++ Magic-Ball-8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic-Ball-8
+//
+//  Created by karthik sagar on 4/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Magic-Ball-8UITests/Magic_Ball_8UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Magic_Ball_8UITestsLaunchTests.swift
+//  Magic-Ball-8UITests
+//
+//  Created by karthik sagar on 4/9/24.
+//
+
+import XCTest
+
+final class Magic_Ball_8UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
